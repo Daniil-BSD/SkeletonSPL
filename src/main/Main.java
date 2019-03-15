4022ab93c9ca1459235743cd56f1b25ae51c0708
CYux <37713574+CYux@users.noreply.github.com>
2019-03-15 12:18:48 +0100

    Revert "Update Main.java"
    System.out.println("I, Alen, have commited a change!");

--- src/main/Main.java
+++ src/main/Main.java
@@ -15,7 +15,6 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("I, Daniil, have commited a change!");
-		System.out.println("I, Alen, have commited a change!");
 	}
 
 }
